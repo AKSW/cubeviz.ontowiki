ccd86730cde9494c0cebae820389124ce858c9cf
Konrad Abicht <k.abicht@googlemail.com>
2012-05-22 20:52:31 +0200

    Init first files (CubevizController and default.ini)

---
+++ CubevizController.php
@@ -0,0 +1,23 @@
+<?php
+
+class CubevizController extends OntoWiki_Controller_Component {
+    
+    public function init () {
+        parent::init();   
+    }
+    
+    /**
+     * 
+     */
+    public function indexAction () {
+        //
+    }
+    
+    /**
+     * 
+     */
+    public function sayhelloAction () {
+        // disable autorendering for this action only
+        $this->_helper->viewRenderer->setNoRender();
+    }
+}
