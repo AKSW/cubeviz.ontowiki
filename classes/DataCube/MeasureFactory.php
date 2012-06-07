d1b63f440a2df9e60a0df83d12e7cf4914279e85
Konrad Abicht <k.abicht@googlemail.com>
2012-06-07 11:14:13 +0200

    Clear some classes and trasform these into ArrayObject subclasses

--- classes/DataCube/MeasureFactory.php
+++ classes/DataCube/MeasureFactory.php
@@ -2,7 +2,7 @@
 /**
  * This class represents a MeasureFactory
  *
- * @copyright Copyright (c) 2011, {@link http://aksw.org AKSW}
+ * @copyright Copyright (c) 2012, {@link http://aksw.org AKSW}
  * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
  * @category OntoWiki
  * @package Extensions
@@ -10,17 +10,16 @@
  * @author Ivan Ermilov
  * @author Konrad Abicht
  */
- 
-require_once CUBEVIZ_ROOT . DS . 'classes' . DS . 'DataCube' . DS . 'Measure.php';
-
 class DataCube_MeasureFactory
 {    
-	public $measures = array();
-
 	public function __construct() {
-		
+		$this ['measures'] = array ();
 	}
 	
+    /**
+     * @param $selectedMeasures 
+     * @todo change selectedMeasures -> measures 
+     */
 	public function initFromArray($selectedMeasures) {
 		$selectedMeasures_length = sizeof($selectedMeasures["measures"]);
 		while($selectedMeasures_length--) {
@@ -28,7 +27,7 @@ class DataCube_MeasureFactory
 			$measure = new DataCube_Measure($current_measure['uri'], $current_measure['md5'], $current_measure['label'], 
 										    $current_measure['type'], $current_measure['order'], $current_measure['aggregationMethod'],
 										    $current_measure['roundValues'], $current_measure['orderDirection']);
-			array_push($this->measures, $measure);
+			array_push($this ['measures'], $measure);
 		}
 	}
 }
