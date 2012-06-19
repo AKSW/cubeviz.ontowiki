2ce104fbe219e547db891f71f44998a2a7207767
Konrad Abicht <k.abicht@googlemail.com>
2012-06-19 21:05:26 +0200

    Add dummy Chart class which represents the mother of all HighCharts Chart's

---
+++ static/javascript/frontend/org/aksw/CubeViz/ChartTransformer/HighCharts/Chart.js
@@ -0,0 +1,10 @@
+Namespacedotjs('org.aksw.CubeViz.ChartTransformer.HighCharts.Chart', {
+    
+    /**
+     * @param rawData JSON object from server (result of an SPARQL query)
+     * @return
+     */
+    getChartInput: function () {
+         
+    }
+});
