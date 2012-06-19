1933189f8338877e4c015495703276ed66c1315d
Konrad Abicht <k.abicht@googlemail.com>
2012-06-19 21:06:06 +0200

    Add HighCharts.js which contains Exception function to create Exceptions

---
+++ static/javascript/frontend/org/aksw/CubeViz/ChartTransformer/HighCharts.js
@@ -0,0 +1,10 @@
+Namespacedotjs('org.aksw.CubeViz.ChartTransformer.HighCharts', {
+    
+    /**
+     * Represents an Exceptions, especially for HighCharts
+     */
+    Exception: function (message) {
+        this.name = "HighCharts Exception";
+        this.message = message || "";
+    }
+});
