e3dff02b30a898969eec1fa27b9300965ab19a32
Konrad Abicht <k.abicht@googlemail.com>
2012-10-12 17:03:35 +0200

    Fix incorrect computation of the height of the container div

--- static/javascript/frontend/org/aksw/CubeViz/UserInterface/IndexAction.js
+++ static/javascript/frontend/org/aksw/CubeViz/UserInterface/IndexAction.js
@@ -11,8 +11,12 @@ Namespacedotjs('org.aksw.CubeViz.UserInterface.IndexAction', {
         /**
          * Set height of container div
          */
-        $("#container").css ( "height", ( screen.height - 205 ) );
-        $("#wrapper-index").css ( "height", ( screen.height - 205 ) );
+         
+        var containerHeight = $("#container").offset(); 
+        containerHeight = ( screen.height - containerHeight.top - 55 );
+         
+        $("#container").css ( "height", containerHeight );
+        $("#wrapper-index").css ( "height", containerHeight );
         
         $('.tabs').html ("");
     },
