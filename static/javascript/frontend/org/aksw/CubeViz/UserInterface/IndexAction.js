45118205236386329d21b6a7b3005e236b69b381
kriss <kriss@kriss-x40.(none)>
2012-10-19 15:26:20 +0200

    Added newer ChartSelector.

--- static/javascript/frontend/org/aksw/CubeViz/UserInterface/IndexAction.js
+++ static/javascript/frontend/org/aksw/CubeViz/UserInterface/IndexAction.js
@@ -21,38 +21,40 @@ Namespacedotjs('org.aksw.CubeViz.UserInterface.IndexAction', {
      * 
      */
     setupChartSelectionEvent: function () {
-        
-        // After click on an item in chartSelection, reload the chart 
+        // After click on an item in chartSelection, reload the chart
         $('.chartSelectionItem').click (function(event) {
-            
             Namespacedotjs.include('org.aksw.CubeViz.Controller.Main');
             var CubeViz_Controller_Main = org.aksw.CubeViz.Controller.Main;
             
             CubeViz_Controller_Main.selectedChartClass = event.target.name;
+			$(".chartSelector-item").removeClass("current");
+			$(event.target).parent().addClass("current");
             $(body).trigger("AjaxResultObservationsRetrieved.CubeViz");
         });
-        
     },
     
     /**
      * 
      */
 	updateChartSelection: function ( suiteableCharts ) {
-        $("#chartSelection").html ("");
-        
-        var iconPath = "", name = "";
-        
+        if($("#chartSelection").html())
+			return;
+        var iconPath = "", name = "", item = "", icon = "";
         $.each(suiteableCharts.charts, function (index, element) {
-            
             iconPath = CubeViz_Parameters_Component.cubevizImagesPath + element.icon;
             name = element ["class"];
-                        
-            $("#chartSelection").append (
-                "<img src=\"" + iconPath + "\" name=\"" + name + "\" class=\"chartSelectionItem\">"
-            );
-            
+			item = $("<div></div>").addClass("chartSelector-item");
+			icon = $("<img/>").attr({
+				"src": iconPath,
+				"name": name,
+				"class": "chartSelectionItem"
+			}).appendTo(item);
+			item.appendTo($("#chartSelection"));
         });
-        
-        $(body).trigger ("UserInterface.IndexAction.ChartSelectionComplete");
+		$("#chartSelection").addClass("chartSelector");
+        Namespacedotjs.include('org.aksw.CubeViz.UserInterface.ChartSelector');
+		ChartSelector.onItemSelect = function(nr){}
+		ChartSelector.init(0);
+        $(body).trigger("UserInterface.IndexAction.ChartSelectionComplete");
     }
 });
