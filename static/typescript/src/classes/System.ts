b44216feaee834e0a2d7ce99fe1ae7ee5e3812fb
Konrad Abicht <k.abicht@googlemail.com>
2012-10-21 13:49:53 +0200

    Delete call and rand function from System.ts, add countProperties

--- static/typescript/src/classes/System.ts
+++ static/typescript/src/classes/System.ts
@@ -2,39 +2,37 @@
 /// <reference path="..\DeclarationSourceFiles\JSON.d.ts" />
 
 class System {
+    
+    /**
+     * Counts number of an given object.
+     */
+    static countProperties ( obj:Object ) : number {
+        var keyCount = 0, k = null;
+        for (k in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, k)) {
+                ++keyCount;
+            }
+        }
+        return keyCount;
+    }
+    
+    /**
+     * Copy an given element, but renew the reference so there is no connection to the old one.
+     */
+    static deepCopy ( elementToCopy ) {
+        var newElement = $.parseJSON ( JSON.stringify ( elementToCopy ) );
+        return newElement;
+    }
+    
+    /**
+     * Outputs only if context is "development"
+     * Should prevent you running into errors if browser doesnt support console.log
+     */
     static out ( output: any ) : void {
         if ( typeof console !== "undefined" 
               && typeof console.log !== "undefined" 
               && "development" == CubeViz_Config.context ) {
             console.log ( output );
         }
-    }
-    
-    /**
-     * Calls a function f with any parameters
-     */
-    static call ( f, param? ) {
-        if ( typeof f !== "undefined" ) {
-            if ( typeof param !== "undefined" ) {
-                eval ( "f (param);" );
-            } else {
-                f ();
-            }
-        }
-    }
-    
-    /**
-     * http://phpjs.org/functions/rand/
-     */
-    static rand () {
-        return Math.floor(Math.random() * (2147483647 + 1));
-    }
-    
-    /**
-     * 
-     */
-    static deepCopy ( elementToCopy ) {
-        var newElement = $.parseJSON ( JSON.stringify ( elementToCopy ) );
-        return newElement;
     }
 }
