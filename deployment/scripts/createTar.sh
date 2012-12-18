ca76d7cc324f50f5b00479374c2ca386914782db
Konrad Abicht <k.abicht@googlemail.com>
2012-12-18 17:08:15 +0100

    Move debian folder to deployment/debian and 2 missing scripts

---
+++ deployment/scripts/createTar.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Set variables
+cubevizRoot="$PWD"
+packageName="cubeviz"
+
+
+# Start the action ...
+echo ""
+echo "- Delete previously created tar-package folder in generated-packages"
+rm -rf $cubevizRoot/deployment/generated-packages/$packageName
+
+echo ""
+echo "- Create tar-package folder in generated-packages"
+mkdir $cubevizRoot/deployment/generated-packages/$packageName
+
+echo ""
+echo "- Copy files to tar-package folder:"
+echo ""
+rsync -av --exclude='.git' \
+		  --exclude='assets' \
+		  --exclude='data' \
+		  --exclude='debian' \
+		  --exclude='deployment' \
+		  --exclude='additional-files' \
+		  --exclude='ChartConfig.js' \
+		  --exclude='data' \
+		  --exclude='generated-packages' \
+		  --exclude='Makefile' \
+		  --exclude='scripts' \
+		  --exclude='static/typescript' \
+		  --exclude='README.md' \
+          $cubevizRoot/* $cubevizRoot/deployment/generated-packages/$packageName
+
+echo ""
+echo "- Copy files (README, adapted Makefile, ChartConfig.js)"
+mv $cubevizRoot/deployment/generated-packages/$packageName/LICENSE.md          $cubevizRoot/deployment/generated-packages/$packageName/LICENSE.txt
+cp $cubevizRoot/deployment/additional-files/Makefile                           $cubevizRoot/deployment/generated-packages/$packageName
+cp $cubevizRoot/README.md                                                      $cubevizRoot/deployment/generated-packages/$packageName/README.txt
+mv $cubevizRoot/deployment/generated-packages/$packageName/ChartConfig.js.dist $cubevizRoot/deployment/generated-packages/$packageName/ChartConfig.js
+
+echo ""
+echo "- Generate archive"
+cd deployment/generated-packages && rm -f $packageName.tar.gz && tar czf $packageName.tar.gz $packageName
+
+echo ""
+echo "- Remove folder cubeviz"
+rm -rf cubeviz
+
+echo ""
