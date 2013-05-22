fc90e5dcf6edc1c4c4497e20c65ca4af5a6edb52
Konrad Abicht <k.abicht@googlemail.com>
2013-05-22 17:18:14 +0200

    Show menu entry only if a model was selected

--- CubevizHelper.php
+++ CubevizHelper.php
@@ -10,6 +10,11 @@ class CubevizHelper extends OntoWiki_Component_Helper
      */
     public function __construct() 
     {
+        // In case no model was selected, show no menu entry
+        if (false === isset(OntoWiki::getInstance()->selectedModel)) {
+            return;
+        }
+        
         $owApp      = OntoWiki::getInstance();
         $translate  = $owApp->translate;
         $url        = new OntoWiki_Url(array('controller' => 'cubeviz', 'action' => 'analyze'), array());
